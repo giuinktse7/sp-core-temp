7f26a95c117482f0a8279046e5398d73252d7587
Jonathan Krän <jonathan.kran1@gmail.com>
2018-08-28 18:24:37 +0200

    Working asset serving

--- server/app/sp/controllers/Application.scala
+++ server/app/sp/controllers/Application.scala
@@ -2,27 +2,21 @@ package sp.controllers
 
 import javax.inject._
 import play.twirl.api.Html
-
 import sp.shared.SharedMessages
 import play.api.mvc._
 import controllers._
 import controllers.Assets.Asset
-
 import javax.inject.Inject
+import play.api.ApplicationLoader.Context
+import play.api.i18n.I18nComponents
+import play.api.{Application, ApplicationLoader, BuiltInComponentsFromContext}
 import play.api.routing.Router.Routes
-import play.api.routing.SimpleRouter
+import play.api.routing.{Router, SimpleRouter}
 import play.api.routing.sird._
+import play.filters.HttpFiltersComponents
+
 
 @Singleton
 class Application @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
-
-def versioned(s: String) = controllers.routes.Assets.versioned(s)
-  def index = Action(Ok(AssetsHelper.index2("SP-Core", versioned)))
-}
-
-class TestRouter extends SimpleRouter {
-  override def routes: Routes = {
-    case GET(p"/versionedAssets/$file") => Assets.versioned(file)
-    case GET(p"/assets/$file") => Assets.at(file)
-  }
+  def index = Assets.at("index.html")
 }
