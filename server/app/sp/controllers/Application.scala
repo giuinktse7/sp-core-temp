0387add891f9260cbb7ff5df5e4b25b5914b5cc4
Jonathan Krän <jonathan.kran1@gmail.com>
2018-08-28 16:42:36 +0200

    Add SIRD routes

--- server/app/sp/controllers/Application.scala
+++ server/app/sp/controllers/Application.scala
@@ -8,6 +8,11 @@ import play.api.mvc._
 import controllers._
 import controllers.Assets.Asset
 
+import javax.inject.Inject
+import play.api.routing.Router.Routes
+import play.api.routing.SimpleRouter
+import play.api.routing.sird._
+
 @Singleton
 class Application @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
@@ -15,4 +20,10 @@ def versioned(s: String) = controllers.routes.Assets.versioned(s)
   def index = Action(Ok(AssetsHelper.index("SP-Core", versioned(_))))
   def serve(file: Asset) = controllers.Assets.versioned(path="/public", file: Asset)
 
+}
+
+class TestRouter extends SimpleRouter {
+  override def routes: Routes = {
+    case GET(p"/") => Action(Results.Ok("Ok"))
+  }
 }
